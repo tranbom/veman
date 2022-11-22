0be1ece2ff86a37e5daec25639de8c7cdae0390c
Mikael Tranbom <mikael@tranbom.io>
2022-11-22 19:17:20 +0100

    Bump version

--- veman/_version.py
+++ veman/_version.py
@@ -15,4 +15,4 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-__version__ = '0.0.2'
+__version__ = '0.0.3'
